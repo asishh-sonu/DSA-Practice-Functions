1f3c1cbf96b42ccb31a485ef644f4a752ebb2010
Asish Kumar Barik <107698745+asishh-sonu@users.noreply.github.com>
2024-08-02 19:13:16 +0530

    Add files via upload

---
+++ FunctionPratice.java
@@ -0,0 +1,8 @@
+public class FunctionPratice{
+    public static void Pal() {
+        System.out.println("CSK is going to loose today");
+    }
+    public static void main(String args[]){
+        Pal();
+    }
+}
